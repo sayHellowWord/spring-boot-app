edfbdb12558f854307282880f176e45c9c03114c
jiangmengyi <1260501552@qq.com>
2016-12-04 16:59:01 +0800

    多任务推送数据

--- src/main/java/com/yd/schedule/ScheduledTasks.java
+++ src/main/java/com/yd/schedule/ScheduledTasks.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
@@ -16,23 +15,75 @@ import java.util.Random;
 @Component
 public class ScheduledTasks {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    private static int id = 10000;
+    private static int id = 1000000;
+
+    private static int logs1 = 300000000;
+
+    private static int logs2 = 500000000;
+
+    private static int logs3 = 700000000;
+
+    private static int logs4 = 900000000;
+
 
     @Autowired
     ExampleService exampleService;
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        System.out.println("现在时间：" + dateFormat.format(new Date()));
         LogStash logStash = new LogStash();
         logStash.setId(++id);
-        logStash.setUserId(new Random().nextInt(100000) + "");
+        logStash.setUserId("mac-" + new Random().nextInt(100000) + "");
         logStash.setGroupId(new Random().nextInt(999999999));
         logStash.setVoteId(new Random().nextInt(999999999));
         logStash.setCreateTime(new Date());
         exampleService.insertLogstash(logStash);
     }
+//
+//    @Scheduled(fixedRate = 5000)
+//    public void logs1() {
+//        LogStash logStash = new LogStash();
+//        logStash.setId(logs1);
+//        logStash.setUserId("logs1-" + new Random().nextInt(100000));
+//        logStash.setGroupId(new Random().nextInt(999999999));
+//        logStash.setVoteId(new Random().nextInt(999999999));
+//        logStash.setCreateTime(new Date());
+//        exampleService.insertLogstash(logStash);
+//    }
+//
+//    @Scheduled(fixedRate = 5000)
+//    public void logs4() {
+//        LogStash logStash = new LogStash();
+//        logStash.setId(logs4);
+//        logStash.setUserId("logs4-" + new Random().nextInt(100000));
+//        logStash.setGroupId(new Random().nextInt(999999999));
+//        logStash.setVoteId(new Random().nextInt(999999999));
+//        logStash.setCreateTime(new Date());
+//        exampleService.insertLogstash(logStash);
+//    }
+//
+//    @Scheduled(fixedRate = 5000)
+//    public void logs2() {
+//        LogStash logStash = new LogStash();
+//        logStash.setId(logs2);
+//        logStash.setUserId("logs2-" + new Random().nextInt(100000));
+//        logStash.setGroupId(new Random().nextInt(999999999));
+//        logStash.setVoteId(new Random().nextInt(999999999));
+//        logStash.setCreateTime(new Date());
+//        exampleService.insertLogstash(logStash);
+//    }
+//
+//    @Scheduled(fixedRate = 5000)
+//    public void logs3() {
+//        LogStash logStash = new LogStash();
+//        logStash.setId(logs3);
+//        logStash.setUserId("logs3-" + new Random().nextInt(100000));
+//        logStash.setGroupId(new Random().nextInt(999999999));
+//        logStash.setVoteId(new Random().nextInt(999999999));
+//        logStash.setCreateTime(new Date());
+//        exampleService.insertLogstash(logStash);
+//    }
+
 
 }
